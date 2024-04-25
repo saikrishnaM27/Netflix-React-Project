5d1cdaf6d4e4a57cfda65318147b5c99304c1c89
saikrishnaM27 <saikrishnamaggidi27@gmail.com>
2024-04-25 16:38:01 +0530

    commited

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,9 +2,9 @@ const SignUp = () => {
     return (
         <>
             <div className="w-50 getStartedForm m-auto">
-                <form className="input-group" method="GET">
+                <form className="input-group" method="POST">
                     <input type="email" name="email" placeholder="Email address" className="form-control form-control-lg bg-transparent text-white" required />
-                    <button type="submit" className="btn btn-danger fw-bold">Get Started &gt;</button>
+                    <button type="button" className="btn btn-danger fw-bold">Get Started &gt;</button>
                 </form>
             </div>
         </>
